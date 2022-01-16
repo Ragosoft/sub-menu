702102d44c8ba391afc8c8ab4878f30e4ee9ab93
Ragosoft <97634656+Ragosoft@users.noreply.github.com>
2022-01-16 12:29:22 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 let isMobile = {
-	Android: function() {return navigator.userAgent.match(/Android/i);},
-	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
-	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
-	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
-	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
-	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+	Android: function () { return navigator.userAgent.match(/Android/i); },
+	BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
+	iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+	Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
+	Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
+	any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
 };
-		let body=document.querySelector('body');
-if(isMobile.any()){
+let body = document.querySelector('body');
+//if(isMobile.any()){
 		body.classList.add('touch');
 		let arrow=document.querySelectorAll('.arrow');
 	for(i=0; i<arrow.length; i++){
@@ -21,6 +21,6 @@ if(isMobile.any()){
 			thisArrow.classList.toggle('active');
 		});
 	}
-}else{
-	body.classList.add('mouse');
-}
+//}else{
+	//body.classList.add('mouse');
+//}
